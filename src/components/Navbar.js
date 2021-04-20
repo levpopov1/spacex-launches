3b61769d066d6e43143da6fa98c29a8b931e5b72
Lev Popov <levpopov1@gmail.com>
2021-04-20 15:23:35 +0100

    adds navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav className="navbar navbar-light mb-3">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          <img src="/spacex-logo.png" alt="SpaceX Logo"/>
+          <span className="text-center">LAUNCHES</span>
+        </a>
+        <button className="btn btn-primary">Reload</button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
