c91512bc5328bb693b214bd8f31f498dfa64e0c6
Lev Popov <levpopov1@gmail.com>
2021-04-20 16:12:56 +0100

    adds api fetch function for all launches

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useState, useEffect } from 'react';
+
+const API_URL = "https://api.spacexdata.com/v4/";
+
 function App() {
+
+  const [launches, setLaunches] = useState({});
+  const [fetchError, setFetchError] = useState({isError: false, message: ""});
+
+  useEffect(() => {
+    const getLaunches = async () => {
+      setFetchError({isError: false});
+      try {
+        let response = await fetch(API_URL + "launches");
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        let data = await response.json();
+        console.log(data);
+        setLaunches(data);
+      } catch (error) {
+        setFetchError({isError: true, message: error.message});
+      }
+    }
+    getLaunches();
+  }, []);
+
   return (
     <div className="App">
-      
+
     </div>
   );
 }
