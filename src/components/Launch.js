a080f5d7dc59509d5fa411391535def381600bad
Lev Popov <levpopov1@gmail.com>
2021-04-21 00:10:28 +0100

    adds handler for rocket and date update

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -1,13 +1,42 @@
-function Launch({launch, index}) {
+import { useState, useEffect } from 'react';
+import nth from '../lib/FormatDate';
+
+function Launch({launch, rockets, index}) {
+
+  const [rocket, setRocket] = useState({});
+  const [launchDate, setLaunchDate] = useState("");
+
+  useEffect(() => {
+    let rkt = rockets.find(rocket => launch.rocket === rocket.id);
+    setRocket(rkt);
+  }, [launch, rockets]);
+
+  useEffect(() => {
+    let ld = new Date(launch.date_utc);
+    let day = ld.getDate();
+    let fullDate = day + nth(day) + " " + ld.toLocaleDateString('default', { month: 'short', year: 'numeric' });
+    setLaunchDate(fullDate);
+  }, [launch]);
+
   return (
-    <div className="launch card">
-      <div className="card-body">
-        <h1 className="card-title">
-          #{index+1}
-        </h1>
-        <h1 className="card-title">
-          {launch.name}
-        </h1>
+    <div className="launch card mb-3">
+      <div className="card-body d-flex flex-row justify-content-between">
+        <div className="launch-name d-flex flex-row">
+          <h1 className="card-title mb-0">
+            #{index+1}
+          </h1>
+          <h1 className="card-title ms-5 mb-0">
+            {launch.name}
+          </h1>
+        </div>
+        <div className="launch-info text-end">
+          <p className="text mb-0">
+            {launchDate}
+          </p>
+          <h4 className="card-title mb-0">
+            {rocket.name}
+          </h4>
+        </div>
       </div>
     </div>
   );
