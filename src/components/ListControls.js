b71ee05c4e1d1d2a46acf8debe4e4264c8fd1ba9
Lev Popov <levpopov1@gmail.com>
2021-04-21 16:23:17 +0100

    adds sort handler

--- src/components/ListControls.js
+++ src/components/ListControls.js
@@ -1,9 +1,29 @@
-function ListControls() {
+import { useState } from 'react';
+
+function ListControls({launches, setLaunches}) {
+
+  const [sortOrder, setSortOrder] = useState("descending");
+  
+  const handleSort = (order) => {
+    let sorted = doSort([...launches], order);
+    setLaunches(sorted);
+    if(sortOrder === "descending"){
+      setSortOrder("ascending");
+    }
+    else{
+      setSortOrder("descending");
+    }
+  }
+
+  const doSort = (collection, order) => {
+    return collection.sort((a, b) => order === 'ascending' ? a.flight_number - b.flight_number : b.flight_number - a.flight_number);
+  }
+
   return (
     <div className="row mb-3">
       <div className="col-sm-12 d-flex justify-content-end">
         <button className="btn btn-primary ms-2">Filter By Year</button>
-        <button className="btn btn-primary ms-2">Sort Descending</button>
+        <button className="btn btn-primary ms-2" onClick={() => handleSort(sortOrder)}>Sort {sortOrder}</button>
       </div>
     </div>
   );
