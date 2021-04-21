0249c58d4531f942bab5c4da2979e70187cb6b40
Lev Popov <levpopov1@gmail.com>
2021-04-21 19:01:39 +0100

    moves filterControl to separate component

--- src/components/ListControls.js
+++ src/components/ListControls.js
@@ -1,37 +1,11 @@
-import { useState, useEffect } from 'react';
+import FilterControl from './FilterControl';
 import SortControl from './SortControl';
 
 function ListControls({launches, setFilteredLaunches}) {
-
-  const [years, setYears] = useState([]);
-  
-  useEffect(() => {
-    let yearFilters = getFilters(launches);
-    console.log(yearFilters);
-    setYears(yearFilters);
-    return () => {
-      setYears([]);
-    }
-  }, [launches]);
-
-  const getFilters = (collection) => {
-    return [...new Set(collection.map(item => new Date(item.date_utc).getFullYear()))];
-  }
-
   return (
     <div className="row mb-3">
       <div className="col-sm-12 d-flex justify-content-end">
-        <div className="dropdown">
-          <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownFilterByYear" data-bs-toggle="dropdown" aria-expanded="false">
-            Filter By Year
-          </button>
-          <ul className="dropdown-menu" aria-labelledby="dropdownFilterByYear">
-            <li><button className="dropdown-item" onClick={() => setFilteredLaunches({type: 'reset', payload: launches})}>All Years</button></li>
-            {
-              years.map(year => <li key={year}><button className="dropdown-item" onClick={() => setFilteredLaunches({type: 'filter', payload: year})}>{year}</button></li>)
-            }
-          </ul>
-        </div>
+        <FilterControl launches={launches} setFilteredLaunches={setFilteredLaunches}/>
         <SortControl setFilteredLaunches={setFilteredLaunches}/>
       </div>
     </div>
