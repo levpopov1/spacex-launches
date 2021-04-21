51b926685fdd09f69b0d94df4934a6f340471f1d
Lev Popov <levpopov1@gmail.com>
2021-04-21 01:55:46 +0100

    changes getLanches to query

--- src/App.js
+++ src/App.js
@@ -9,28 +9,58 @@ const API_URL = "https://api.spacexdata.com/v4";
 function App() {
 
   const [launches, setLaunches] = useState([]);
-  const [fetchError, setFetchError] = useState({isError: false, message: ""});
+  const [fetchingError, setFetchingError] = useState({isError: false, message: ""});
 
   useEffect(() => {
     const getLaunches = async () => {
-      setFetchError({isError: false});
+
+      setFetchingError({isError: false});
+
+      let requestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          "query":{},
+          "options": {
+            "select": {
+              "name": 1,
+              "rocket": 1,
+              "date_utc": 1,
+              "flight_number": 1
+            },
+            "populate": [
+              {
+                "path": "rocket",
+                "select": {
+                  "name": 1,
+                }
+              }
+            ]
+          }
+        })
+      }
+
       try {
-        let response = await fetch(API_URL + "/launches");
+        let response = await fetch(API_URL + "/launches/query", requestOptions);
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
         let data = await response.json();
         console.log(data);
-        setLaunches(data);
+        setLaunches(data.docs);
       } catch (error) {
-        setFetchError({isError: true, message: error.message});
+        setFetchingError({isError: true, message: error.message});
       }
     }
-    const getLaunchesLocal = () => {
-      console.log("fetching from local");
-      setLaunches(localData);
-    }
-    getLaunchesLocal();
+
+    // const getLaunchesLocal = () => {
+    //   console.log("fetching from local");
+    //   setLaunches(localData);
+    // }
+
+    getLaunches();
   }, []);
 
   return (
@@ -43,7 +73,11 @@ function App() {
             <img src="/img/launch-home.png" className="launch-img" alt="Rocket Launch"/>
           </div>
           <div className="col-sm-7">
-            <Launches launches={launches} API_URL={API_URL}/>
+            {
+              fetchingError.isError
+              ? <div>An error occured: <pre>fetchingError.message</pre></div>
+              : <Launches launches={launches} API_URL={API_URL}/>
+            }
           </div>
         </div>
       </div>
