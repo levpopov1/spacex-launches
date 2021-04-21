e87fde9d3a1c5fd695f36459491e56d180334be7
Lev Popov <levpopov1@gmail.com>
2021-04-21 18:15:05 +0100

    lifts filterLaunches reducer up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 import Launches from './components/Launches';
 import ListControls from "./components/ListControls";
 import Navbar from "./components/Navbar";
@@ -8,7 +8,23 @@ const API_URL = "https://api.spacexdata.com/v4";
 
 function App() {
 
+  function init(initialState) {
+    return initialState;
+  }
+  
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'filter':
+        return launches.filter(launch => new Date(launch.date_utc).getFullYear() === action.payload);
+      case 'reset':
+        return init(action.payload);
+      default:
+        throw new Error();
+    }
+  }
+
   const [launches, setLaunches] = useState([]);
+  const [filteredLaunches, setFilteredLaunches] = useReducer(reducer, launches, init);
   const [fetchingError, setFetchingError] = useState({isError: false, message: ""});
 
   useEffect(() => {
@@ -50,6 +66,7 @@ function App() {
         let data = await response.json();
         console.log(data);
         setLaunches(data.docs);
+        setFilteredLaunches({type: 'reset', payload: data.docs})
       } catch (error) {
         setFetchingError({isError: true, message: error.message});
       }
@@ -67,7 +84,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <div className="container">
-        <ListControls launches={launches} setLaunches={setLaunches}/>
+        <ListControls launches={launches} setLaunches={setLaunches} setFilteredLaunches={setFilteredLaunches}/>
         <div className="row">
           <div className="col-sm-5">
             <img src="/img/launch-home.png" className="launch-img" alt="Rocket Launch"/>
@@ -76,7 +93,7 @@ function App() {
             {
               fetchingError.isError
               ? <div>An error occured: <pre>fetchingError.message</pre></div>
-              : <Launches launches={launches} API_URL={API_URL}/>
+              : <Launches launches={filteredLaunches} API_URL={API_URL}/>
             }
           </div>
         </div>
