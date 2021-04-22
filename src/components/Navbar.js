f904444d949e69632d68607cccdfb9f3edac07fe
Lev Popov <levpopov1@gmail.com>
2021-04-22 17:44:41 +0100

    moves datareload to separate component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,27 @@
-function Navbar() {
+import { useState } from 'react';
+import ReloadData from "./ReloadData";
+
+function Navbar({makeAPIRequest, setFetchingError, setLaunches, setFilteredLaunches}) {
+
+  const [showReloadMessage, setShowReloadMessage] = useState(false);
+
   return (
     <nav className="navbar navbar-light mb-3">
       <div className="container">
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand d-flex" href="/">
           <img src="spacex-logo.png" alt="SpaceX Logo"/>
-          <span className="text-center">LAUNCHES</span>
+          LAUNCHES
         </a>
-        <button className="btn btn-primary">Reload</button>
+        <div className="controls d-flex flex-row align-items-center">
+          {showReloadMessage && <p className="text text-success fw-bold mb-0 me-3">Data Reloaded!</p>}
+          <ReloadData 
+            makeAPIRequest={makeAPIRequest} 
+            setFetchingError={setFetchingError} 
+            setLaunches={setLaunches} 
+            setFilteredLaunches={setFilteredLaunches} 
+            setShowReloadMessage={setShowReloadMessage}
+          />
+        </div>
       </div>
     </nav>
   );
